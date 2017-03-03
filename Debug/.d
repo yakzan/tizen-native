33617e9d372d0d9e392b670a1865167472fc27af
bilgin218 <bilgin218@gmail.com>
2017-03-03 14:45:34 +0200

    Adds the native sensor code with sampling frequency of 100 Hz

---
+++ Debug/.d
@@ -0,0 +1,26 @@
+res/edje/data.edj: ../res/edje/../../inc/view_defines.h \
+  ../res/edje/edc_defines.h \
+  ./../res/images/background_2.png \
+  ./../res/images/main/background_application.png \
+  ./../res/images/examples/background_examples.png \
+  ./../res/images/indicator/indicator_notselected.png \
+  ./../res/images/background_2.png \
+  ./../res/images/indicator/indicator_selected.png \
+  ./../res/images/main/background_frame.png \
+  ./../res/images/information/button_data_prev.png \
+  ./../res/images/examples/play_button_normal.png \
+  ./../res/images/information/button_data_next.png \
+  ./../res/images/examples/play_button_hoover.png \
+  ./../res/images/background_2_frame.png \
+  ./../res/images/background_2_frame.png \
+  ./../res/images/main/main_indicator1_on.png \
+  ./../res/images/main/main_indicator2_on.png \
+  ./../res/images/main/main_indicator3_on.png \
+  ./../res/images/main/indicator_dot.png \
+  ./../res/images/main/line.png \
+  ./../res/images/main/menu_frame.png \
+  ./../res/images/main/menu_text_examples.png \
+  ./../res/images/main/menu_text_data.png \
+  ./../res/images/main/menu_text_information.png \
+  ./../res/images/main/button_hover.png \
+  ./../res/images/main/button_normal.png
